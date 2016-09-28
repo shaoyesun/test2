2a3a34210c5c64528315b3a39288ab0487508bd7
sunjf <sunjf@biomarker.com.cn>
2016-09-28 15:37:07 +0800

    controller add success

--- src/main/java/com/controller/UserController.java
+++ src/main/java/com/controller/UserController.java
@@ -1,7 +1,18 @@
 package com.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
 /**
  * Created by root on 16-9-28.
  */
+@Controller
+@RequestMapping(value = "/user")
 public class UserController {
+
+    @RequestMapping(value = "/login")
+    public String login(){
+        return "success";
+    }
+
 }
