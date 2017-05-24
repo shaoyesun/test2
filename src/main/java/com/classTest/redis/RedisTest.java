c7caa55bc62220f0477681e90a7e336ecce0e6ad
git <sunjf@biomarker.com.cn>
2017-05-24 09:33:38 +0800

    session

--- src/main/java/com/classTest/redis/RedisTest.java
+++ src/main/java/com/classTest/redis/RedisTest.java
@@ -1,7 +1,9 @@
 package com.classTest.redis;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import redis.clients.jedis.Jedis;
 
@@ -11,7 +13,7 @@ import redis.clients.jedis.Jedis;
 public class RedisTest {
 
     public static void main(String[] args) {
-        Jedis jedis = new Jedis("localhost");
+        Jedis jedis = new Jedis("127.0.0.1", 6379);
         System.out.println("Connection to server sucessfully");
         //查看服务是否运行
         System.out.println("Server is running: "+jedis.ping());
@@ -24,6 +26,22 @@ public class RedisTest {
         for(int i=0; i<list.size(); i++) {
             System.out.println("List of stored keys:: "+list.get(i));
         }
+
+        //redis操作Map
+        hmset(jedis);
+    }
+
+    //redis操作Map
+    public static void hmset(Jedis jedis){
+        Map<String, String> map = new HashMap<String, String>();
+        map.put("name", "jedis");
+        map.put("age", "22");
+        map.put("qq", "jedis");
+        jedis.hmset("user",map);
+        //取出user中的name，执行结果:[minxr]-->注意结果是一个泛型的List
+        //第一个参数是存入redis中map对象的key，后面跟的是放入map中的对象的key，后面的key可以跟多个，是可变参数
+        List<String> rsmap = jedis.hmget("user", "name", "age", "qq");
+        System.out.println(rsmap);
     }
 
 
